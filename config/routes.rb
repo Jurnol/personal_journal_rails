7eb3d94c9d28582daaa6771efa035e3439ecdb91
Antoine Aube <aaube3@gmail.com>
2024-01-14 15:46:59 -0700

    routes: create user route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #user registration and session endpoints
+  post "/api/v1/register", to: "api/v1/users#create"
 end
