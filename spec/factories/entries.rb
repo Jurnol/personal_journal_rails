2462a6d13c006eb9b1433268ab7fcc9b25552c83
Antoine Aube <aaube3@gmail.com>
2024-01-14 15:19:20 -0700

    test: model tests for journal entries +/-

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :entry do
-    journal { nil }
-    title { "MyString" }
-    content { "MyText" }
+    journal
+    title { Faker::Book.title }
+    content { Faker::Lorem.paragraph(sentence_count: 2) }
     entry_date { "2024-01-14" }
   end
 end
--- spec/factories/journals.rb
+++ spec/factories/journals.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :journal do
-    title { "MyString" }
-    status { "MyString" }
-    entry_count { 1 }
-    image { "MyString" }
-    user { nil }
+    title { Faker::Book.title}
+    status { Journal.statuses.values.sample }
+    entry_count { 0 }
+    description { Faker::Lorem.sentence(word_count: 5) }
+    image { Faker::LoremFlickr.image(size: "50x60") }
+    user
   end
 end
