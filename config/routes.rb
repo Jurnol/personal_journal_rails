0a39c8bfe8413a54cce0dadb28a4cf9ed309cfa1
Antoine Aube <aaube3@gmail.com>
2024-01-22 13:17:21 -0700

    fix: issue with register route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do 
-      post "register", to: "users#create"
+      post "/register", to: "users#create"
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
     end
