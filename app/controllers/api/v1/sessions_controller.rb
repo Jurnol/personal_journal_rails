e95268e13cd584c7abaab1e6d2064f8d345e4758
Antoine Aube <aaube3@gmail.com>
2024-01-22 13:18:07 -0700

    feature: create sessions action with encode JWT token private method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user &.authenticate(params[:password])
+      token = encode_token({user_id: user.id})
+      render json: { token: token }, status: :ok
+    else 
+      render json: { error: "Invalid Credentials"}, status: :unauthorized
+    end
+  end
+
+
+  private
+
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.credentials.jwt_secret_key)
+  end
 end
