88e5e7546e2ec5d986f26e5564bfe07a38ba4670
Antoine Aube <aaube3@gmail.com>
2024-01-22 13:18:28 -0700

    app controller method for decoding token for subsequent requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def decoded_token
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+    begin
+      JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, {algorithm: 'HS256'})
+    rescue JWT::DecodeError
+      nil
+    end
+  end
 end
