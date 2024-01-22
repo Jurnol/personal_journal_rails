ce320dc866a327af08ffbed776ca7db750a48b0d
Antoine Aube <aaube3@gmail.com>
2024-01-22 13:51:13 -0700

    create autheticate user method for JWT, refactor decoded method to be private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
+
+private 
+
   def decoded_token
     header = request.headers['Authorization']
     token = header.split(' ').last if header
@@ -7,6 +10,16 @@ class ApplicationController < ActionController::Base
       JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, {algorithm: 'HS256'})
     rescue JWT::DecodeError
       nil
+    end
+  end
+
+  def authenticate_user
+    decoded = decoded_token
+
+    if decoded_token
+      @current_user = User.find_by(id: decoded[0]['user_id'])
+    else 
+      render json: {error: 'Unauthorized'}, status: :unauthorized
     end
   end
 end
