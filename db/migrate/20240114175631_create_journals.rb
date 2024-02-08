170f02024fb97eadb24b6997260c0ff7a6a5a96d
Antoine Aube <aaube3@gmail.com>
2024-02-08 13:30:12 -0700

    db_schema: adjustments for 'active' default value for journal status to be 1

--- db/migrate/20240114175631_create_journals.rb
+++ db/migrate/20240114175631_create_journals.rb
@@ -2,7 +2,7 @@ class CreateJournals < ActiveRecord::Migration[7.0]
   def change
     create_table :journals do |t|
       t.string :title
-      t.string :status, default: 1
+      t.integer :status, default: 1
       t.integer :entry_count, default: 0
       t.string :image
       t.references :user, null: false, foreign_key: true, index: true
--- db/migrate/20240114214608_change_status_type_in_journals.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStatusTypeInJournals < ActiveRecord::Migration[7.0]
-  def up
-    change_column :journals, :status, 'integer USING CAST(status AS integer)'
-  end
-
-  def down
-    change_column :journals, :status, :string
-  end
-end
