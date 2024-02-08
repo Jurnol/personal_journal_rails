bfa2917dc9ece41690172b083158f0db46b0a3be
Antoine Aube <aaube3@gmail.com>
2024-02-08 11:07:07 -0700

    fix: session controller tests to have new data format coming from vue 'userData'

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe "Api::V1::Sessions", type: :request do
   describe "POST create a session" do
     it "creates a session and return a JWT token for the user session" do 
       @user_1 = User.create!(first_name: "guy", last_name: "fieri", email: "guy@gmail.com", password: "guy", password_confirmation: "guy")
-      post "/api/v1/login", params: {email: "guy@gmail.com", password: "guy"}
-      
+      # require 'pry';binding.pry
+      post "/api/v1/login", params: {userData: {email: "guy@gmail.com", password: "guy"} }
 
       expect(response).to be_successful
       expect(response.status).to be(200)
@@ -17,13 +17,12 @@ RSpec.describe "Api::V1::Sessions", type: :request do
       token = response_body[:token]
 
       decoded_token = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, { algorithm: "HS256" })
-      require 'pry';binding.pry
       expect(decoded_token.first["user_id"]).to eq(@user_1.id)
     end
 
     it "returns an error message if the wrong password is entered" do 
       @user_1 = User.create!(first_name: "guy", last_name: "fieri", email: "guy@gmail.com", password: "guy", password_confirmation: "guy")
-      post "/api/v1/login", params: {email: "guy@gmail.com", password: "wrongpassword"}
+      post "/api/v1/login", params: { userData: {email: "guy@gmail.com", password: "wrongpassword"} }
 
       expect(response).to_not be_successful
 
