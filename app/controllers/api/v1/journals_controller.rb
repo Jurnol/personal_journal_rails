64ef565834a7d5a155f72647bd8fd488edfda8f9
Antoine Aube <aaube3@gmail.com>
2024-02-08 19:42:07 -0700

    modify user data to not return the password digest..wooops

--- app/controllers/api/v1/journals_controller.rb
+++ app/controllers/api/v1/journals_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::JournalsController < ApplicationController
   def index
     user = @current_user
       if user
-        render json: {user_info: user, user_journals: user.journals}, status: 200
+        render json: {user_info: { first_name: user.first_name, last_name: user.last_name, email: user.email }, user_journals: user.journals}, status: 200
       end
   end
 end
